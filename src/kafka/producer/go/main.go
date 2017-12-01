23cef466f9e0a25ccfe3d62dfafe6a2cfe6126a3
Andy Kirkham <andy.kirkham@solarwinds.com>
2017-12-01 10:30:39 +0000

    Conf Go producer via env vars

--- src/kafka/producer/go/main.go
+++ src/kafka/producer/go/main.go
@@ -4,26 +4,21 @@ import (
 	"fmt"
 	"os"
 	"time"
-	kingpin "gopkg.in/alecthomas/kingpin.v2"
 	"github.com/Shopify/sarama"
 )
 
-var (
-	brokerList = kingpin.Flag("brokerList", "List of brokers to connect").Default("localhost:9092").Strings()
-	topic      = kingpin.Flag("topic", "Topic name").Default("test").String()
-)
-
 func main() {
-	kingpin.Parse()
 	name, err := os.Hostname()
 	if err != nil {
 		panic(err)
 	}
+	brokers := []string{ os.Getenv("KAFKA_BROKER") }
+	topic := os.Getenv("KAFKA_TOPIC")
 	config := sarama.NewConfig()
 	config.Producer.RequiredAcks = sarama.WaitForAll
 	config.Producer.Retry.Max = 5
 	config.Producer.Return.Successes = true
-	producer, err := sarama.NewSyncProducer(*brokerList, config)
+	producer, err := sarama.NewSyncProducer(brokers, config)
 	if err != nil {
 		panic(err)
 	}
@@ -34,14 +29,14 @@ func main() {
 	}()
 	for {
 		msg := &sarama.ProducerMessage{
-			Topic: *topic,
+			Topic: topic,
 			Value: sarama.StringEncoder(name),
 		}
 		partition, offset, err := producer.SendMessage(msg)
 		if err != nil {
 			panic(err)
 		}
-		fmt.Printf("Message is stored in topic(%s)/partition(%d)/offset(%d)\n", *topic, partition, offset)
+		fmt.Printf("Message is stored in topic(%s)/partition(%d)/offset(%d)\n", topic, partition, offset)
 		duration := time.Second
 		time.Sleep(duration)
 	}
